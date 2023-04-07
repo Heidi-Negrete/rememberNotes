57a4dd539294a1d6b5f1bef7343bca091e771a11
Heidi Negrete <heidi@negrete.dev>
2023-04-07 12:16:07 -0400

    create RememberNotes class for module

--- scripts/remember-notes.js
+++ scripts/remember-notes.js
@@ -1,1 +1,11 @@
-console.log('remember-notes | Hello World!');
+class RememberNotes {
+  static ID = 'remember-notes';
+
+  static FLAGS = {
+    MEMOS: 'memos'
+  }
+
+  static TEMPLATES = {
+    REMEMBERNOTES: `modules/${this.ID}/templates/remember-notes.hbs`
+  }
+}

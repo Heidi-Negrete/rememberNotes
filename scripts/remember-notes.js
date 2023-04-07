14b198d9aac4b7efeaf2554ce2e4f5dbfa8e1461
Heidi Negrete <heidi@negrete.dev>
2023-04-07 12:33:55 -0400

    define create and read API methods for MemoData

--- scripts/remember-notes.js
+++ scripts/remember-notes.js
@@ -15,10 +15,29 @@ class MemoData {
   static get allMemos() {}
 
   // get all memos for a given user
-  static getMemoForUser(userId) {}
+  static getMemoForUser(userId) {
+    return game.users.get(userId)?.getFlag(RememberNotes.ID, RememberNotes.FLAGS.MEMOS);
+  }
 
   // create a new memo for a given user
-  static createMemo(userId, memoData) {}
+  static createMemo(userId, memoData) {
+    // generate random id for new Memo and populate userID
+    const newMemo = {
+      isPopUp: false,
+      ...memoData,
+      id: foundry.utils.randomID(16),
+      userId,
+      label: "" //label?
+    }
+
+    // construct the update to insert new Memo
+    const newMemos = {
+      [newMemo.id]: newMemo
+    }
+
+    // update database with new Memos
+    return game.users.get(userId)?.setFlag(RememberNotes.id, RememberNotes.FLAGS.MEMOS, newMemos);
+  }
 
   // update a specific memo by id with the provided updateData
   static updateMemo(memoID, updateData) {}

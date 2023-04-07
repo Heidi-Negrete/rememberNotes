4550f1abaa9f50869b71d38cd142dbebd7fad7ac
Heidi Negrete <heidi@negrete.dev>
2023-04-07 12:20:07 -0400

    add MemoData class for accessing and editing memos

--- scripts/remember-notes.js
+++ scripts/remember-notes.js
@@ -9,3 +9,20 @@ class RememberNotes {
     REMEMBERNOTES: `modules/${this.ID}/templates/remember-notes.hbs`
   }
 }
+
+class MemoData {
+  // all memos for all users
+  static get allMemos() {}
+
+  // get all memos for a given user
+  static getMemoForUser(userId) {}
+
+  // create a new memo for a given user
+  static createMemo(userId, memoData) {}
+
+  // update a specific memo by id with the provided updateData
+  static updateMemo(memoID, updateData) {}
+
+  // delete a specific memo by id
+  static deleteMemo(memoId) {}
+}
